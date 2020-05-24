7d07e2b9aa336009bb580e1f52a09d065e055b63
Sandy Herho <herholabs@protonmail.ch>
2020-05-25 06:45:46 +0700

    tambah

---
+++ matlab-crash/myscript.m
@@ -0,0 +1,6 @@
+clear all; close all; clc
+
+display('Juliet: O Romeo, Romeo, wherefore art thou Romeo?')
+display('Romeo: Shall I hear more, or shall I speak at this?')
+
+
---
+++ sumintup.m
@@ -0,0 +1,7 @@
+function sumintup(N)
+    total = 0;
+    for n = 1:5
+        total = total + n;
+    end
+    fprintf('total equals %d\n', total);
+end
