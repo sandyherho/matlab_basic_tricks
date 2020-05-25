7556c5ac411aad5e0da49788fe3d95ea3eae8c74
Sandy Herho <herholabs@protonmail.ch>
2020-05-25 23:42:22 +0700

    tes

---
+++ rand_check.m
@@ -0,0 +1,15 @@
+function rand_check
+    N = 5;
+    list = rand(1, N);
+
+    for x = list
+
+        if x > 0.5
+            fprintf('Random number of %f is large.\n', x);
+        else
+            fprintf('Random number %f is small.\n', x);
+        end
+
+    end
+
+end
